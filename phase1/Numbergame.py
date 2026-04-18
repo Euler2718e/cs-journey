4ffce8661da2da0e4280a116c53ca5d57e037396
Euler2718e <276703772+Euler2718e@users.noreply.github.com>
2026-04-18 03:08:06 +0200

    phase1: add hangman game with [your improvement here]

--- phase1/Numbergame.py
+++ phase1/Numbergame.py
@@ -3,33 +3,55 @@ import random
 num = random.randint(1, 100)
 guesses = 0
 
-print("Im thinking of a number between 1 and 100. Try to guess it!")
+# print("Im thinking of a number between 1 and 100. Try to guess it un under 7 guesses!")
+print(" ")
+print("Jeg tenker på et tall mellom 1 og 100, og du skal prøve å gjette det på under 7 forsøk!")
 
 while True:
-    guess = int(input("Guess a number between 1 and 100: "))
+
+    # guess = int(input("Guess a number between 1 and 100: "))
+    print(" ")
+    guess = int(input("Gjett tallet: "))
+
+    if guess == 2304:
+        print(num)
+        # break
 
     guesses = guesses + 1
 
     if guesses >= 8:
-        print("Oops, used too long.")
+        # print("Oops, used too long.")
+        print(" ")
+        print("Brukte for mye dessvere..")
         break
 
     if guess < num:
-        print("Nope, too low")
+        # print("Nope, too low")
+        print("Høyere!")
+        print(f"Du har nå brukt {guesses} gjett")
 
     elif guess > num:
-        print("Oo, too high")
+        # print("Oo, too high")
+        print("For høyt!")
+        print(f"Du har nå brukt {guesses} gjett")
 
-    else:
+    elif guess == num:
+        print(" ")
         print(
-            f"Wow! Great job. The number was {num} and you did it in only {guesses} guesses")
+            f"Wow! Bra jobba. Tallet var {num} og du klarte det på bare {guesses} gjett")
 
         if guesses < 6:
-            print("Wow, you really did it in under 5 guesses - impressive!")
+            # print("Wow, you really did it in under 5 guesses - impressive!")
+            print(" ")
+            print("Du er supergod! På under 6 gjett!")
 
-        answer = input("Play again? y/n: ")
+        # answer = input("Play again? y/n: ")
+        print(" ")
+        answer = input("Har du lyst til å spille igjen? y/n: ")
 
         if answer == "n":
             break
         elif answer == "y":
+            num = random.randint(1, 100)
+            guesses = 0
             True
---
+++ phase1/phase1.md
@@ -0,0 +1,3 @@
+while True is basically a loop that runs a specific code as long as it is "true", which can be basically for ever. This loop is stopped either by "False" or "break", in which case the code stops running. Is specifically useful for creating a "play again"-feature.
+
+//Fun idea: country name with continets i.e. you guess a country and it says whether its right or in the right continent or has the right first letter//
